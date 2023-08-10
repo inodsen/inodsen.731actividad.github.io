c647207f5b2e5bec1ca72ca4f885ba3f0a75b508
inodsen <127694763+inodsen@users.noreply.github.com>
2023-08-10 18:52:08 -0300

    foour commit
    actividad 731

---
+++ registro/js/validacion.js
@@ -0,0 +1,19 @@
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
+function pass (contra1, contra2){
+    
+    let boolean = false;
+    
+    if (contra1 == contra2);
+    {
+       boolean = true;
+    return boolean; 
+    } 
+}
+
